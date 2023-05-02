98b316a4a20658d4bc3d4a3fd598a226f7c6872a
Luna <69774917+carlyjb17@users.noreply.github.com>
2023-05-02 00:44:00 +0000

    Yayyy

--- get_next_line.c
+++ get_next_line.c
@@ -1,89 +1,44 @@
 #include "get_next_line.h"
 
-static int ft_check(char *s)
-{
-    int ret;
-    
-    ret = 0;
-    while (*s)
-    {
-        if (*s == '\n')
-        {
-            while (*s)
-            {
-                s++;
-                ret++;
-            }
-            if (ret == 0)
-                ret = 1;
-            return (ret);
-        }
-        s++;
-    }
-    return(0);
-}
-
 char    *get_next_line(int fd)
 {
-    char *buffer;
-    static char *rest;
+    static char *sobras;
     char *ret;
-    int diff_size;
+    char *buff;
+    int index;
 
-    if (fd < 0)
-        return(NULL);
-    if (rest)
-	{
-        ret = ft_strdup(rest);
-		ret++;
-	}
+    if (sobras != NULL)
+        ft_memcpy(buff, sobras, sizeof(sobras));
     else
     {
-        ret = malloc((BUFFER_SIZE + 1) * sizeof(char));
-        if (read(fd, ret, BUFFER_SIZE) == -1)
-        {
-            free(ret);
+        buff = malloc(BUFFER_SIZE * sizeof(char) + 1);
+        if (!buff)
             return(NULL);
-        }
-    }
-    if (!ret)
-        return (NULL);
-    buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-    if (!buffer)
-    {
-        free(ret);
-        return(NULL);
-    }
-    while (1)
-    {
-        if (ft_check(ret) == 1)
-            break;
-        if (ft_check(ret) > 1)
+        if (read(fd, buff, BUFFER_SIZE) == -1)
         {
-            diff_size = ft_check(ret);
-            rest = malloc((diff_size + 1) * sizeof(char));
-			//printf("(%s %i %lu)", ret, diff_size, ft_strlen(ret));
-            rest = ft_substr(ret, (ft_strlen(ret) - diff_size), diff_size);
-            if (!rest)
+            free(buff);
+            return (NULL);
+        }
+        sobras = malloc(BUFFER_SIZE * sizeof(char) + 1);
+    }   
+    index = 0;
+    //Comprueba si hay salto de linea
+    while (buff[index])
+    {
+        if (buff[index] == '\n')
+        {
+            ret = malloc(index * sizeof(char) + 1);
+            if (!ret)
             {
-                free(buffer);
-                free(ret);
+                free(buff);
+                free(sobras);
                 return(NULL);
             }
-            ret = ft_substr(ret, 0, (ft_strlen(ret) - diff_size + 1));
-            break;
+            ft_memcpy(ret, buff, index * sizeof(char));
+            ft_memcpy(sobras, buff, ft_strlen(buff+index));
+            free(buff);
+            return(ret);
         }
-        if (!read(fd, buffer, BUFFER_SIZE))
-            break;
-        ret = ft_strjoin(ret, buffer);
+        index++;
     }
-    free(buffer);
-    if (!ret)
-    {
-        if (rest)
-            free(rest);
-        return(NULL);
-    }
-    ret[ft_strlen(ret)] = '\0';
-    return(ret);
 }
